a1e215edd7a9a4c19fb5fcea13c978da2db35b19
Burak Ince <burak.ince@linux.org.tr>
2018-05-28 01:10:21 +0200

    Add CORS

--- main.go
+++ main.go
@@ -28,8 +28,27 @@ type JSONWebKeys struct {
 	X5c []string `json:"x5c"`
 }
 
+func corsMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+		c.Writer.Header().Set("Access-Control-Max-Age", "86400")
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE, UPDATE")
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "Origin, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
+		c.Writer.Header().Set("Access-Control-Expose-Headers", "Content-Length")
+		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+
+		if c.Request.Method == "OPTIONS" {
+			c.AbortWithStatus(200)
+		} else {
+			c.Next()
+		}
+	}
+}
+
 func setupRouter() *gin.Engine {
 	r := gin.Default()
+
+	r.Use(corsMiddleware())
 
 	//r.Use(cors.New(cors.Config{
 	//	AllowOrigins:     []string{"*"},
